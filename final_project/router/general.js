c7c61fb1839982fcac3aeb13b182f03f132e90e1
KenichiroArai <sammyleejunior@gmail.com>
2024-11-12 23:48:35 -0500

    タスク1

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,10 +11,12 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get('/',
+    function (req, res) {
+        let result = res.send(JSON.stringify(books, null, 4));
+        return result;
+    }
+);
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
